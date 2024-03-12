7fd2a126fec8639714e677fd69b0b35373a7b114
mati0ri <mateo.iori223@gmail.com>
2024-03-12 14:58:42 +0100

    Initial Commit

---
+++ Festival du Jeu IOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Festival du Jeu IOS
+//
+//  Created by Mateo iori on 12/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Festival du Jeu IOS/Festival_du_Jeu_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Festival_du_Jeu_IOSApp.swift
+//  Festival du Jeu IOS
+//
+//  Created by Mateo iori on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Festival_du_Jeu_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
