051ef6c79bc1abcfc2247b826d4b610344ae52d9
Lénaïs Desbos <lenaisdesbos@MacBook-Air-de-Lenais.local>
2024-03-16 16:25:32 +0100

    Plus de retour possible une fois connecté, Lien vers inscription préparé

--- Festival du Jeu IOS/ContentView.swift
+++ Festival du Jeu IOS/ContentView.swift
@@ -9,22 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
-                NavigationLink(destination: LoginView()) {
-                    Text("Aller à la page de connexion")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                }
-            }
-            .padding()
-        }
+        
+            LoginView()
+        
     }
 }
 
